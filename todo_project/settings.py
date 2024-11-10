0505d76c58ca8b35203c50201272948c931df118
Rob8624 <robert_melen@yahoo.co.uk>
2024-11-10 20:51:07 +0000

    postgress setup

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        
     }
 }
 

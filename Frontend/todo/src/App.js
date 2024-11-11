abbb3b8c311e2a0b463c6053bdfc5ba695f5a703
Rob8624 <robert_melen@yahoo.co.uk>
2024-11-11 00:23:23 +0000

    reverted back to live db

--- Frontend/todo/src/App.js
+++ Frontend/todo/src/App.js
@@ -1,7 +1,21 @@
 
 import './App.css';
+import React, { useEffect, useState } from "react"
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+
+    const apiURL = 
+  
+  
+  }, [])
+
+
+
+
   return (
     <div className="message">Um......I think we better start packing! We are on the move!</div>
   )

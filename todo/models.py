2dd896b53bb8ee41b4c51de0a1663fef9aa543a3
Rob8624 <robert_melen@yahoo.co.uk>
2024-11-12 10:11:55 +0000

    added choices field to todo model

--- todo/models.py
+++ todo/models.py
@@ -2,10 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
+
+    OWNER_CHOICES = [
+        ('rob', 'Rob'),
+        ('beth', 'Beth'),
+        ('nigel', 'Nigel'),
+        ('joy', 'Joy'),
+        ('joe', "Joe"),
+        (' rob and beth', "Rob and Beth"),
+        ('nigel and joy', "Nigel and Joy"),
+        ('anyone', "Anyone")]
+
     title = models.CharField(max_length=500, blank=True)
     description = models.CharField(max_length=500, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
+    owner = models.CharField(max_length=100, choices=OWNER_CHOICES, default='anyone')
+    
 
     def __str__(self) -> str:
         return self.title

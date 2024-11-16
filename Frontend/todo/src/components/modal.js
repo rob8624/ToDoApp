bffe0d5258aa80cdf45f8a1c1dd6771af646e903
Rob8624 <robert_melen@yahoo.co.uk>
2024-11-16 01:41:38 +0000

    added priority to model

--- Frontend/todo/src/components/modal.js
+++ Frontend/todo/src/components/modal.js
@@ -9,24 +9,26 @@ const owners = todos.length > 0 ? todos[0].owners_choices : [];
 
 return (
     <div className="modal-overlay">
-        <div className="modal-flex">
-            <div className="modal-text">Add the Todo</div>
-            <form className="modal-form-flex">
-                    <label htmlFor="ownerSelect">Select Owner</label>
-                    <select id="ownerSelect">
-                        {
-                           owners.map((item) => (
-                            <option value={item}>{item[0]}</option>
-                           ))
-                        }
-                    </select>
-                
-                    <div className="modal-btn-flex">
-                        <button className="modal-btn-add" type="submit">Add</button>
-                        <button className="modal-btn-close" onClick={() => setShowModal(false)}>Cancel</button>
-                    </div>
-                
-            </form>
+        <div className="modal-border">
+            <div className="modal-flex">
+                <div className="modal-text">Add the Todo</div>
+                <form className="modal-form-flex">
+                        <label htmlFor="ownerSelect">Select Owner</label>
+                        <select id="ownerSelect">
+                            {
+                            owners.map((item) => (
+                                <option value={item}>{item[1]}</option>
+                            ))
+                            }
+                        </select>
+                    
+                        <div className="modal-btn-flex">
+                            <button className="modal-btn-add" type="submit">Add</button>
+                            <button className="modal-btn-close" onClick={() => setShowModal(false)}>Cancel</button>
+                        </div>
+                    
+                </form>
+            </div>
         </div>
     </div>
     )
--- todo/models.py
+++ todo/models.py
@@ -12,12 +12,19 @@ class Todo(models.Model):
         (' rob and beth', "Rob and Beth"),
         ('nigel and joy', "Nigel and Joy"),
         ('anyone', "Anyone")]
+    
+    PRIORITY_CHOICES =[
+        ('high', 'High'),
+        ('medium', 'Medium'),
+        ('low', 'Low'),
+    ]
 
     title = models.CharField(max_length=500, blank=True)
     description = models.CharField(max_length=500, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
     owner = models.CharField(max_length=100, choices=OWNER_CHOICES, default='anyone')
+    priority = models.CharField(max_length=100, choices=PRIORITY_CHOICES, default='high')
     
 
     def __str__(self) -> str:
